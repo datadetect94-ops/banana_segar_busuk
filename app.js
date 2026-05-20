14fa1f5ddd5b9d8de0000b1a817a52efae10aa53
datadetect94-ops <datadetect94@gmail.com>
2026-05-20 10:12:05 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,14 +7,14 @@ const CONFIG = {
     
     // GANTI INI dengan nama kelas kalian. 
     // PERHATIAN: Urutannya HARUS SAMA PERSIS dengan urutan di Roboflow!
-    labels: ["pisang segar", "pisang busuk"], 
+    labels: ["pisang busuk", "pisang segar"], 
     
     // Batas keyakinan AI (0.45 = 45%). 
     // Jika AI terlalu sering salah tebak, naikkan angkanya (misal 0.60).
-    threshold: 0.45,
+    threshold: 0.67,
     
     // Batas untuk menghapus kotak deteksi yang menumpuk (Biarkan saja 0.4)
-    iouThreshold: 0.4
+    iouThreshold: 0.67
 };
 
 // ======================================================================
